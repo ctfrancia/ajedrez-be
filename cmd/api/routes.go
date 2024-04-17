42e4d469695fdae0e50c2c6f15f33ae63ccbcd28
Christian Francia <ctfrancia@gmail.com>
2024-04-17 09:53:10 +0200

    refactoring shiz

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (app *application) routes() *gin.Engine {
+	r := gin.Default()
+
+	r.Use(app.enableCORS())
+	r.Use(app.rateLimit())
+	r.Use(app.authenticate())
+	v1U := r.Group("/v1/user")
+	v1T := r.Group("/v1/tournament")
+	v1C := r.Group("/v1/club")
+	v1Tokens := r.Group("/v1/tokens")
+	v1Sys := r.Group("/v1/system")
+
+	// User routes
+	// v1U.GET("/all", app.getAllUsers)
+	v1U.POST("/create", app.createNewUser)
+	v1U.GET("/:email", app.getUserByEmail)
+	v1U.PUT("/update/", app.updateUser)
+	v1U.DELETE("/delete/:email", app.deleteUser)
+	v1U.PUT("/activated", app.activateUser)
+
+	// Tournament routes
+	v1T.POST("/create", createNewTournament)
+
+	// Club routes
+	// TODO: the middleware below is just for POC, it should be removed
+	v1C.Use(app.requireActivatedUser())
+	v1C.POST("/create", app.createNewClub)
+	v1C.GET("/by-name/:name", app.getClubByName)
+	// v1C.GET("/by-code/:code", app.getClubByCode)
+
+	// Token routes
+	v1Tokens.POST("/authentication", app.createAuthenticationToken)
+
+	// System routes
+	v1Sys.GET("/healthcheck", app.healthcheck)
+
+	return r
+}
