dedcbf2875873c60ebe9ae29c9e73283bf87e6c0
Christian Francia <ctfrancia@gmail.com>
2024-04-28 19:53:23 +0200

    wip

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,25 @@
 package repository
 
 import (
+	"errors"
 	"gorm.io/gorm"
 )
 
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrNoResultSet    = errors.New("no rows in result set")
+)
+
 type Repository struct {
-	DB *gorm.DB
+	// Users UsersRepository
+	// Clubs ClubsRepository
+	// Tokens TokensRepository
+	Tournaments TournamentsRepository
+	// Matches MatchesRepository
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return Repository{
+		Tournaments: TournamentsRepository{db},
+	}
 }
